4d8eea6565474f413c138d257bc7267febcee9d7
414190799 <31713192+414190799@users.noreply.github.com>
2018-02-05 20:53:13 -0600

    Add files via upload
    hw2

---
+++ baojiajun-hw2/baojiajun-hw2/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  baojiajun-hw2
+//
+//  Created by Bao, Jiajun on 2/5/18.
+//  Copyright © 2018 Bao, Jiajun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet var city: UIView!
+    @IBOutlet var message: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+   
+    
+    
+}
+
